e827a249a122fd36ef615535f242b83166b45497
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2023-01-16 21:16:47 +0900

    [프로그래머스] Lv0. 세균 증식

---
+++ 프로그래머스/Lv0. 세균 증식.swift
@@ -0,0 +1,13 @@
+//
+//  Lv0. 세균 증식.swift
+//  Algorithm
+//
+//  Created by 모상현 on 2023/01/16.
+//
+import Foundation
+
+func solution(_ n:Int, _ t:Int) -> Int {
+    return n * Int(pow(2.0 , 15.0))
+}
+
+print(solution(7,15))
