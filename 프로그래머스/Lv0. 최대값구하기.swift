f69eb310f45c5c4f5f5ec0a79301542937fa2d74
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2023-01-24 00:00:18 +0900

    [프로그래머스] Lv0. 최대값구하기(2)

---
+++ 프로그래머스/Lv0. 최대값구하기.swift
@@ -0,0 +1,28 @@
+//
+//  Lv0. 최대값구하기.swift
+//  Algorithm
+//
+//  Created by 모상현 on 2023/01/23.
+//
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> Int {
+    var numberArray = numbers
+    var sumMinValue = 0
+    var sumMaxValue = 0
+    guard let minValue = numberArray.min() else { return 0 }
+    numberArray.remove(at: numberArray.firstIndex(of: minValue) ?? 0)
+    guard let secondMin = numberArray.min() else { return 0 }
+    sumMinValue = minValue * secondMin
+    
+    guard let maxValue = numberArray.max() else { return 0 }
+    numberArray.remove(at: numberArray.firstIndex(of: maxValue) ?? 0)
+    guard let secondMax = numberArray.max() else { return 0 }
+    sumMaxValue = maxValue * secondMax
+    
+    return sumMinValue > sumMaxValue ? sumMinValue : sumMaxValue
+}
+
+print(solution([0, -31, 24, 10, 1, 9]))
+print(solution([1, 2, -3, 4, -5]))
